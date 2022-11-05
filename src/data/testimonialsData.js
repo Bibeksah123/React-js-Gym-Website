56961d1b9141a9adc4cf4144d1b2cb725233da01
BibekKumarSah <bibeksah618@gmail.com>
2022-11-05 16:30:52 +0545

    initial commit

--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -1,25 +1,23 @@
-import image1 from "../assets/t-image1.png";
+import image1 from "../assets/t-image1.jpg";
 import image2 from "../assets/t-image2.jpg";
 import image3 from "../assets/t-image3.jpg";
 
-export const testimonialsData = [
-  {
-    image: image1,
-    review:
-      "I made the right choice by choosing the Fitclub and by choosing the right plan and program I already achieved my ideal body!",
-    name: 'MATHEW HENDRICKSON',
-    status : 'ENTREPRENEUR'
-  },
-  {
-    image: image2,
-    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ipsam, ab itaque nam perferendis impedit sint ',
-    name: 'JOHN KEVIN',
-    status: 'COACH'
-  },
-  {
-    image : image3,
-    review:' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima aspernatur quod voluptatem',
-    name: 'FRANKLIN',
-    status: "CUSTOMER"
-  }
+export const testimonialsData = [{
+        image: image1,
+        review: "I made the right choice by choosing the Fitclub and by choosing the right plan and program I already achieved my ideal body!",
+        name: 'Mr. Bibek sah',
+        status: 'ENTREPRENEUR'
+    },
+    {
+        image: image2,
+        review: 'By selecting the Fitclub and the appropriate plan and program, I made the right decision and have already attained my dream figure! ',
+        name: 'Er. Deepak Shah',
+        status: 'COACH'
+    },
+    {
+        image: image3,
+        review: ' I am so hard on myself and I always strive for being the best. No matter how old I get I will continue working on just getting better. I have a lot to learn but I know that giving up is not an option.',
+        name: 'ER. Ranjit sah',
+        status: "CUSTOMER"
+    }
 ];
